cc5f4e6a97d4b2806346ae4513c5183ae5c391fa
rupinder <Rupinder.Kaur@ssb.no>
2018-05-02 15:55:26 +0200

    Formatted code. Removed extra fields from UI.Removed extra sections from the main page

--- leveranse-frontend/src/components/pages/provisionAgreement/ProvisionAgreement.js
+++ leveranse-frontend/src/components/pages/provisionAgreement/ProvisionAgreement.js
@@ -1,105 +1,105 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
-import { Form, Input } from "semantic-ui-react";
+import {Form, Input} from "semantic-ui-react";
 
 class ProvisionAgreement extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      duration: '',
-      frequency: '',
-      pursuant: '',
-      provisionDate: '',
-      description: '',
-      id: '',
-      localeId: '',
-      name: '',
-      version: '',
-      versionDate: '',
-      versionRationale: '',
-      administrativeDetails: ''
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            duration: '',
+            frequency: '',
+            pursuant: '',
+            provisionDate: '',
+            description: '',
+            id: '',
+            localeId: '',
+            name: '',
+            version: '',
+            versionDate: '',
+            versionRationale: '',
+            administrativeDetails: ''
+        };
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
 
-  handleInputChange (event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+    handleInputChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
 
-  registerProvisionAgreement () {
-    let data = JSON.stringify({
-      duration: this.state.duration,
-      frequency: this.state.frequency,
-      pursuant: this.state.pursuant,
-      provisionDate: null,
-      description: null,
-      id: this.state.id,
-      localeId: null,
-      name: null,
-      version: null,
-      versionDate: null,
-      versionRationale: null,
-      administrativeDetails: {
-        id: null,
-        administrativeStatus: null,
-        alias: null,
-        annotation: null,
-        documentation: null,
-        createDate: null,
-        createBy: null,
-        lastUpdateDate: null,
-        lifeCycleStatus: null,
-        ControlledVocabulary: null,
-        url: null,
-        validFrom: null,
-        validUntil: null,
-        version: null
-      }
-    })
+    registerProvisionAgreement() {
+        const self = this;
+        let data = JSON.stringify({
+            duration: this.state.duration,
+            frequency: this.state.frequency,
+            pursuant: this.state.pursuant,
+            provisionDate: null,
+            description: null,
+            id: this.state.id,
+            localeId: null,
+            name: null,
+            version: null,
+            versionDate: null,
+            versionRationale: null,
+            administrativeDetails: {
+                id: null,
+                administrativeStatus: null,
+                alias: null,
+                annotation: null,
+                documentation: null,
+                createDate: null,
+                createBy: null,
+                lastUpdateDate: null,
+                lifeCycleStatus: null,
+                ControlledVocabulary: null,
+                url: null,
+                validFrom: null,
+                validUntil: null,
+                version: null
+            }
+        })
+        axios.post('http://localhost:8080/api/v1/provisionAgreement', data, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response) {
+            self.setState({id: response.data.id});
+           // console.log(self.state.id);
+        })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
 
-    axios.post('http://localhost:8080/api/v1/provisionAgreement', data, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }
-
-  render () {
-    return (
-      <div>
-        <h3>Leveranseavtale</h3>
-        <Form.Field>
-          <label>Id:</label>
-          <Input placeholder='Id' name='id' value={this.state.id}
-                 onChange={this.handleInputChange}/>
-        </Form.Field>
-        <Form.Field>
-          <label>Varighet:</label>
-          <Input placeholder='Varighet' name='duration' value={this.state.duration}
-                 onChange={this.handleInputChange}/>
-        </Form.Field>
-        <Form.Field>
-          <label>Frekvens:</label>
-          <Input placeholder='Frekvens' name='frequency' value={this.state.frequency}
-                 onChange={this.handleInputChange}/>
-        </Form.Field>
-        <Form.Field>
-          <label>Hjemmel:</label>
-          <Input placeholder='Hjemmel' name='pursuant' value={this.state.pursuant}
-                 onChange={this.handleInputChange}/>
-        </Form.Field>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h3>Leveranseavtale</h3>
+                <Form.Field>
+                    <label>Navn på avtale</label>
+                    <Input placeholder='navn' name='name' value={this.state.name}
+                           onChange={this.handleInputChange}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Varighet</label>
+                    <Input placeholder='Varighet' name='duration' value={this.state.duration}
+                           onChange={this.handleInputChange}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Hyppighet</label>
+                    <Input placeholder='hyppighet' name='frequency' value={this.state.frequency}
+                           onChange={this.handleInputChange}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Hjemmel</label>
+                    <Input placeholder='Hjemmel' name='pursuant' value={this.state.pursuant}
+                           onChange={this.handleInputChange}/>
+                </Form.Field>
+            </div>
+        );
+    }
 }
 
 export default ProvisionAgreement;
--- leveranse-frontend/src/components/pages/role/Role.js
+++ leveranse-frontend/src/components/pages/role/Role.js
@@ -1,74 +1,66 @@
-import React, { Component } from 'react';
-import { Form, Input } from "semantic-ui-react";
+import React, {Component} from 'react';
+import {Form, Input} from "semantic-ui-react";
 import axios from 'axios';
 
 class Role extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      description: '',
-      id: '',
-      localeId: '',
-      name: '',
-      version: '',
-      versionDate: '',
-      versionRationale: '',
-      administrativeDetails: ''
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: '',
+            id: '',
+            localeId: '',
+            name: '',
+            version: '',
+            versionDate: '',
+            versionRationale: '',
+            administrativeDetails: ''
+        };
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
 
-  handleInputChange (event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+    handleInputChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
 
-  registerRole () {
-    let data = JSON.stringify({
-      description: null,
-      id: this.state.id,
-      localeId: this.state.localeId,
-      name: this.state.name,
-      version: null,
-      versionDate: null,
-      versionRationale: null,
-      administrativeDetails: null
-    })
+    registerRole() {
+        let data = JSON.stringify({
+            description: null,
+            id: this.state.id,
+            localeId: this.state.localeId,
+            name: this.state.name,
+            version: null,
+            versionDate: null,
+            versionRationale: null,
+            administrativeDetails: null
+        })
 
-    axios.post('http://localhost:8080/api/v1/role', data, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }
+        axios.post('http://localhost:8080/api/v1/role', data, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
 
-  render () {
-    return (
-      <div>
-        <h3>Rolle</h3>
-        <Form.Field>
-          <label>Navn:</label>
-          <Input placeholder='Navn' name="name" value={this.state.name} onChange={this.handleInputChange}/>
-        </Form.Field>
-        <Form.Field>
-          <label>Id:</label>
-          <Input placeholder='Id' name="id" value={this.state.id} onChange={this.handleInputChange}/>
-        </Form.Field>
-        <Form.Field>
-          <label>Lokal id:</label>
-          <Input placeholder='Lokal id' name="localeId" value={this.state.localeId} onChange={this.handleInputChange}/>
-        </Form.Field>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h3>Rolle</h3>
+                <Form.Field>
+                    <label>Navn</label>
+                    <Input placeholder='Navn' name="name" value={this.state.name} onChange={this.handleInputChange}/>
+                </Form.Field>
+            </div>
+        );
+    }
 }
 
 export default Role;
