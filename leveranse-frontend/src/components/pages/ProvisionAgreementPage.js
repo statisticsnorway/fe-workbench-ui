eb2e33fe3c406ab4af00b911dcf115b74beb87fd
mmj <mmj@ssb.no>
2018-05-30 10:18:53 +0200

    Fetching provisionAgreements and printing the list on the '/home' page. Renamed 'provisionalAgreement -> provisionAgreement'. Made the logo be a button to '/home'. Renamed 'Variable -> variable'.

--- leveranse-frontend/src/components/pages/ProvisionAgreementPage.js
+++ leveranse-frontend/src/components/pages/ProvisionAgreementPage.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Dropdown, Grid, Icon, Input, Menu, Segment } from 'semantic-ui-react'
 import { BrowserRouter, NavLink, Route } from 'react-router-dom'
-import NewProvisionAgreement from './provisionalAgreement/NewProvisionAgreement'
+import NewProvisionAgreement from './provisionAgreement/NewProvisionAgreement'
 import Variable from './variable/Variable'
+import ProvisionAgreementsList from './provisionAgreement/ProvisionAgreementsList'
 import '../../assets/css/site.css'
 
 class ProvisionAgreementPage extends React.Component {
@@ -49,7 +50,7 @@ class ProvisionAgreementPage extends React.Component {
                 <Menu fluid vertical>
                   <Menu.Item header>Variabel</Menu.Item>
                   <Menu.Item name='newVariable' active={activeItem === 'newVariable'}>
-                    <NavLink to='/Variable'>
+                    <NavLink to='/variable'>
                       <Icon name="compose" />
                       Opprett ny
                     </NavLink>
@@ -72,7 +73,8 @@ class ProvisionAgreementPage extends React.Component {
               <Segment inverted tertiary>
                 <Route path='/newProvisionAgreement'
                        render={(props) => <NewProvisionAgreement {...props} isNewProvisionAgreement={true} />} />
-                <Route path='/Variable' component={Variable} />
+                <Route path='/variable' component={Variable} />
+                <Route path='/home' component={ProvisionAgreementsList} />
               </Segment>
             </Grid.Column>
           </Grid>
