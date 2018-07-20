aaa532c2a86a94e0208d471ddb6114d691fbf64a
rupinder <Rupinder.Kaur@ssb.no>
2018-07-20 13:53:17 +0200

    Modifications in sync with the backend changes to fetch list of provisionagreements.

--- leveranse-frontend/src/pages/provisionAgreement/ProvisionAgreementsList.js
+++ leveranse-frontend/src/pages/provisionAgreement/ProvisionAgreementsList.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import axios from 'axios'
-import { Grid, Header, Message, Segment } from 'semantic-ui-react'
+import {Grid, Header, Message, Segment} from 'semantic-ui-react'
 import ReactTable from 'react-table'
 import 'react-table/react-table.css'
+import {getDataFromBackend} from "../../utils/Common";
 
 let selectedProvisionalAgreement
 
 class ProvisionAgreementsList extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       provisionAgreements: [],
@@ -15,134 +15,64 @@ class ProvisionAgreementsList extends React.Component {
       loading: true,
       response: {}
     }
-
-    this.fetchAllStoredProvisionAgreements()
     this.selectProvisionAgreement = this.selectProvisionAgreement.bind(this)
     this.onClickProvisionAgreement = this.onClickProvisionAgreement.bind(this)
   }
 
-  fetchAllStoredProvisionAgreements () {
-    let url
-
-    url = process.env.REACT_APP_BACKENDHOST + process.env.REACT_APP_APIVERSION + '/provisionAgreement'
-
-    axios.get(url).then((response) => {
-      console.log(response)
-      if (response.status === 200) {
-        this.setState({
-          provisionAgreements: response.data
-        })
-      } else {
-        this.setState({
-          response: {
-            header: 'Noe gikk galt ved henting av leveranseavtaler',
-            text: response.statusText + ' (' + url + ')',
-            color: 'yellow',
-            icon: 'warning'
-          }
-        })
-      }
-    }).catch((error) => {
-      console.log(error)
-
-      this.setState({
-        response: {
-          header: 'Noe gikk galt ved henting av leveranseavtaler',
-          text: error.message + ' (' + url + ')',
-          color: 'red',
-          icon: 'warning'
-        }
-      })
-    }).then(() => {
-      this.setState({
+  componentDidMount() {
+    getDataFromBackend('ProvisionAgreement/', this.state.provisionAgreements).then((result) => {
+      this.setState(prevState => ({
+        provisionAgreements: [...prevState.provisionAgreements, result.data],
+        waitingForResponse: false,
         loading: false
-      })
+      }))
     })
   }
 
-  renderCell (row) {
+  renderCell(row) {
     if (row.original.selected === true) {
       return (<div style={{cursor: 'default', fontWeight: 'bold'}}>{row.value}</div>)
     }
-
     return (<div style={{cursor: 'default', fontWeight: 'normal'}}>{row.value}</div>)
   }
 
-  selectProvisionAgreement (e, state, column, rowInfo, instance) {
+  selectProvisionAgreement(e, state, column, rowInfo, instance) {
     if (this.state.selectedIndex != -1) {
       let ProvisionAgreementOld = this.state.provisionAgreements[this.state.selectedIndex]
       ProvisionAgreementOld.selected = false
     }
-
-    let provisionAgreement = this.state.provisionAgreements[rowInfo.index]
+    let provisionAgreement = this.state.provisionAgreements[0][rowInfo.index]
     provisionAgreement.selected = true
 
-    console.log('selected PA: ', provisionAgreement)
     if (provisionAgreement != null) {
       let PA = this.onClickProvisionAgreement(provisionAgreement.id)
     } else {
       console.log('Error retrieving PA from PA list')
     }
-
     this.setState({selectedIndex: rowInfo.index})
   }
 
-  onClickProvisionAgreement (id) {
-    let url
-
-    url = process.env.REACT_APP_BACKENDHOST + process.env.REACT_APP_APIVERSION + '/provisionAgreement/' + id
-
-    axios.get(url).then((response) => {
-      console.log('Fetched PA: ', response)
-
-      selectedProvisionalAgreement = response.data
-
-      if (response.status === 200) {
-        selectedProvisionalAgreement
-      } else {
-        this.setState({
-          response: {
-            header: 'Noe gikk galt ved henting av leveranseavtalen',
-            text: response.statusText + ' (' + url + ')',
-            color: 'yellow',
-            icon: 'warning'
-          }
-        })
-      }
-    }).catch((error) => {
-      console.log(error)
-
-      this.setState({
-        response: {
-          header: 'Noe gikk galt ved henting av leveranseavtalen',
-          text: error.message + ' (' + url + ')',
-          color: 'red',
-          icon: 'warning'
-        }
-      })
-    }).then(() => {
-      this.setState({
-        loading: false
-      })
+  onClickProvisionAgreement(id) {
+    getDataFromBackend('ProvisionAgreement/' + id, this.state.provisionAgreements).then((result) => {
+      selectedProvisionalAgreement = result.data
     })
   }
 
-  render () {
+  render() {
     const {response, loading} = this.state
     const data = this.state.provisionAgreements
-
     const columns = [{
       Header: 'Name',
-      accessor: 'name' // String-based value accessors!
+      accessor: 'name[0].languageText'// String-based value accessors!
     }, {
       Header: 'Description',
-      accessor: 'description'
+      accessor: 'description[0].languageText'
     }, {
       Header: 'Duration From', // Required because our accessor is not a string
-      accessor: 'durationFrom'
+      accessor: 'validFrom'
     }, {
       Header: 'Duration To', // Required because our accessor is not a string
-      accessor: 'durationTo'
+      accessor: 'validTo'
     }]
 
     return (
@@ -156,20 +86,22 @@ class ProvisionAgreementsList extends React.Component {
                   <Message icon={response.icon} color={response.color} header={response.header}
                            content={response.content} /> : null}
                 {loading ? null : <ReactTable
-                  data={data}
+                  data={data[0]}
                   columns={columns}
                   noDataText='No data!'
                   filterable
                   defaultPageSize={10}
                   style={{
-                    height: '400px' // This will force the table body to overflow and scroll, since there is not enough room
+                    height: '400px' //To adjust table height. Adds scroll to the table
                   }}
                   className='-striped -highlight'
                   showPaginationTop
                   showPaginationBottom
                   getTdProps={(state, rowInfo, column, instance) => {
                     return {
-                      onClick: e => { this.selectProvisionAgreement(e, state, column, rowInfo, instance) }
+                      onClick: e => {
+                        this.selectProvisionAgreement(e, state, column, rowInfo, instance)
+                      }
                     }
                   }}
                 />}
