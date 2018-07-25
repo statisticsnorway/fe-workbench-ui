9c989306b42f72883db85148a666b6c71360771b
rupinder <Rupinder.Kaur@ssb.no>
2018-07-25 16:04:14 +0200

    Timeout issue while async call

--- leveranse-frontend/src/__tests__/PADescPage.test.js
+++ leveranse-frontend/src/__tests__/PADescPage.test.js
@@ -1,36 +1,33 @@
 import React from 'react'
-import { ProvisionAgreementDesc } from '../pages/provisionAgreement/ProvisionAgreementDesc'
+import {ProvisionAgreementDesc} from '../pages/provisionAgreement/ProvisionAgreementDesc'
 import {mount, shallow} from 'enzyme'
 import {Checkbox} from 'semantic-ui-react'
 import configureStore from 'redux-mock-store'
 
 describe('verify ProvsiionAgreement Description page', () => {
-  const initialState = {authentication: {user: "mockUser@ssb.no"},
-                        alert: {type: 'success'}}
-  const alert = {type: 'alert-success'}
+  const initialState = {
+    authentication: {user: "mockUser@ssb.no"},
+    alert: {type: 'success'}
+  }
   const mockStore = configureStore()
   let store, container
 
-  beforeEach(() => {
+  beforeEach((done) => {
     store = mockStore(initialState)
     delete console.log
   })
 
-  it('render ProvisionAgreement Description component', () => {
-    container = shallow(<ProvisionAgreementDesc store={store} />)
+  test('render ProvisionAgreement Description component', async (done) => {
+    container = shallow(<ProvisionAgreementDesc store={store}/>)
     expect(container.length).toEqual(1)
-  });
+  }, 500000);
 
-  it('verify edit option is active by default', () => {
-    console.log(alert)
+  test('verify edit option is active by default',  async (done) => {
     container = mount(<ProvisionAgreementDesc store={store}
                                               authentication={initialState}
                                               alert={initialState}
                                               isNewProvisionAgreement={true}/>)
     const editOption = container.find(Checkbox)
-    console.log(editOption.text())
-
-  });
-
+    expect(editOption.text()).toEqual("Redigeringsmodus");
+  }, 500000);
 })
-
