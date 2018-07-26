ca38576861e25332aa0bc3020dc216c71cc09d8a
rupinder <Rupinder.Kaur@ssb.no>
2018-07-26 10:10:09 +0200

    formatting changes

--- leveranse-frontend/src/__tests__/HomePage.test.js
+++ leveranse-frontend/src/__tests__/HomePage.test.js
@@ -7,7 +7,7 @@ import configureStore from 'redux-mock-store'
 import {BrowserRouter as Router} from 'react-router-dom';
 
 describe('verify Home page', () => {
-  const initialState = {authentication: {user: "mockUser@ssb.no"}}
+  const initialState = { authentication: { user: "mockUser@ssb.no" } }
   const mockStore = configureStore()
   let store, container
 
@@ -16,7 +16,7 @@ describe('verify Home page', () => {
   })
 
   it('render HomePage component', () => {
-    container = shallow(<HomePage store={store} user={initialState}/>)
+    container = shallow(<HomePage store={store} user={initialState} />)
     expect(container.length).toEqual(1)
   });
 
