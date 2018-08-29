922f107dbd9a3dcf6cda42764e417bf978371991
oranheim <oranheim@gmail.com>
2018-08-29 18:02:02 +0200

    Added Docker support

---
+++ leveranse-frontend/docker/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#rm -rf node_modules
+
+if [[ ! -d "dc-jsonschema-react-page-builder" ]]; then
+    git clone git@github.com:statisticsnorway/dc-jsonschema-react-page-builder.git
+else
+    cd dc-jsonschema-react-page-builder
+    git pull
+    cd ..
+fi
+
+cat package.json | sed "s,https://github.com/statisticsnorway/dc-jsonschema-react-page-builder.git,file:dc-jsonschema-react-page-builder,g" > package.json.docker
+
+docker build -t dui .
+
+
