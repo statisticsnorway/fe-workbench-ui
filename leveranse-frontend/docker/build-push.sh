0d2151582866498e2a3359fa4055a1d36adeb534
oranheim <oranheim@gmail.com>
2018-09-12 15:18:36 +0200

    Upgraded docker release to 0.0.4

--- leveranse-frontend/docker/build-push.sh
+++ leveranse-frontend/docker/build-push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ./docker/build.sh
-docker tag dc-workbench-ui eu.gcr.io/p2-utvikling/dc-workbench-ui:0.0.2
+docker tag dc-workbench-ui eu.gcr.io/p2-utvikling/dc-workbench-ui:0.0.4
 gcloud auth application-default print-access-token | docker login -u oauth2accesstoken --password-stdin https://eu.gcr.io
-docker push eu.gcr.io/p2-utvikling/dc-workbench-ui:0.0.2
+docker push eu.gcr.io/p2-utvikling/dc-workbench-ui:0.0.4
 
 #docker run -it -p 3000:3000 -e REACT_APP_BACKENDHOST="http://172.17.0.1:9090" eu.gcr.io/p2-utvikling/dc-workbench-ui:0.0.2
