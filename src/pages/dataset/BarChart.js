d46628ffd77c33782649be81e823d7d8c8edc8cb
Igor Erokhin <igor.erokhin@ssb.no>
2019-03-18 11:14:34 +0100

    Use the actual data for the bar chart
    Shows bars for the first data object

--- src/pages/dataset/BarChart.js
+++ src/pages/dataset/BarChart.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 import { Bar } from 'react-chartjs-2';
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'Example dataset',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
-
 export default class BarChart extends Component {
+
+  componentDidMount () {
+    console.log('Data to plot: ', this.props.data)
+  }
+
+  prepareData() {
+    const {data} = this.props
+    const disallowed = ['kommuneregion', 'periode']
+
+    let allowedLabels = Object.keys(data[0])
+      .filter((e) => !disallowed.includes(e))
+
+    return {
+      labels: allowedLabels,
+      datasets: [
+        {
+          label: 'Dyr',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: allowedLabels.map(e => data[0][e])
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Bar Chart example</h2>
         <Bar
-          data={data}
+          data={this.props.data ? this.prepareData() : []}
           width={100}
           height={50}
           options={{maintainAspectRatio: true}}
