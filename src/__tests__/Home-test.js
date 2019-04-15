ac421384d6cb243da74d46b6eb5ffb8d32768fa4
mmj <mmj@ssb.no>
2019-04-15 14:43:57 +0200

    Added ability to show/hide the top menu. Excluded json-files and only specific things from utilities from test coverage.

--- src/__tests__/Home-test.js
+++ src/__tests__/Home-test.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import 'jest-dom/extend-expect'
 import { MemoryRouter } from 'react-router-dom'
-import { cleanup, render } from 'react-testing-library'
+import { cleanup, fireEvent, render } from 'react-testing-library'
 
 import Home from '../pages/home/Home'
 import { getData } from '../utilities/fetch/Fetch'
@@ -11,18 +12,19 @@ import Roles from './test-data/Roles'
 jest.mock('../utilities/fetch/Fetch', () => ({getData: jest.fn()}))
 
 afterEach(() => {
+  getData.mockReset()
   cleanup()
 })
 
 const setup = () => {
   const props = {user: 'test'}
-  const {queryAllByRole, queryAllByTestId, queryAllByText} = render(
+  const {getByTestId, getByText, queryAllByRole, queryAllByTestId, queryAllByText} = render(
     <MemoryRouter>
       <Home {...props} />
     </MemoryRouter>
   )
 
-  return {queryAllByRole, queryAllByTestId, queryAllByText}
+  return {getByTestId, getByText, queryAllByRole, queryAllByTestId, queryAllByText}
 }
 
 test('Home renders correctly', () => {
@@ -35,4 +37,28 @@ test('Home renders correctly', () => {
   expect(queryAllByText(`${UI.LANGUAGE.nb} (${UI.LANGUAGE_CHOICE.nb})`)).toHaveLength(1)
   expect(queryAllByRole('option')).toHaveLength(4)
   expect(queryAllByText(UI.LOGOUT.nb)).toHaveLength(1)
+})
+
+test('Chevron shows/hides top menu', () => {
+  getData.mockImplementation(() => Promise.resolve(Roles[0]))
+
+  const { getByTestId, getByText } = setup()
+
+  fireEvent.click(getByTestId('topMenu-toggle'))
+
+  setTimeout(() => {
+    expect(getByText('SSB Logo')).not.toBeVisible()
+    expect(getByTestId('global-search')).not.toBeVisible()
+    expect(getByText(`${UI.LANGUAGE.nb} (${UI.LANGUAGE_CHOICE.nb})`)).not.toBeVisible()
+    expect(getByText(UI.LOGOUT.nb)).not.toBeVisible()
+  }, 200)
+
+  fireEvent.click(getByTestId('topMenu-toggle'))
+
+  setTimeout(() => {
+    expect(getByText('SSB Logo')).toBeVisible()
+    expect(getByTestId('global-search')).toBeVisible()
+    expect(getByText(`${UI.LANGUAGE.nb} (${UI.LANGUAGE_CHOICE.nb})`)).toBeVisible()
+    expect(getByText(UI.LOGOUT.nb)).toBeVisible()
+  }, 200)
 })
--- src/pages/home/TopMenu.js
+++ src/pages/home/TopMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Divider, Grid, Segment } from 'semantic-ui-react'
+import { Button, Divider, Grid, Segment, Transition } from 'semantic-ui-react'
 
 import Status from './status/Status'
 import UserDropdown from './UserDropdown'
@@ -7,24 +7,35 @@ import SearchField from '../../pages/search/SearchField'
 import { SSBLogo } from '../../media/Logo'
 
 class TopMenu extends Component {
+  state = { visible: true }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
   render () {
-    const {client, handleLogout, user} = this.props
+    const { client, handleLogout, user } = this.props
+    const { visible } = this.state
 
     return (
-      <Segment style={{marginBottom: 0}}>
-        <Grid stackable reversed='mobile'>
-          <Grid.Column only='tablet computer' tablet={11} computer={13}>
-            <Status user={user} />
-          </Grid.Column>
-          <Grid.Column only='tablet computer' tablet={5} computer={3} textAlign='right'>
-            <UserDropdown handleLogout={handleLogout} user={user} />
-            <Divider hidden />
-            {SSBLogo('100%')}
-            <Divider hidden />
-            <SearchField alignement='right' client={client} />
-          </Grid.Column>
-        </Grid>
-      </Segment>
+      <div>
+        <Transition visible={visible} animation='fade down' duration={200}>
+          <Segment style={{ marginBottom: 0 }} attached>
+            <Grid stackable reversed='mobile'>
+              <Grid.Column only='tablet computer' tablet={11} computer={13}>
+                <Status user={user} />
+              </Grid.Column>
+              <Grid.Column only='tablet computer' tablet={5} computer={3} textAlign='right'>
+                <UserDropdown handleLogout={handleLogout} user={user} />
+                <Divider hidden />
+                {SSBLogo('100%')}
+                <Divider hidden />
+                <SearchField alignement='right' client={client} />
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </Transition>
+        <Button attached='bottom' basic icon={visible ? 'chevron up' : 'chevron down'}
+                onClick={this.toggleVisibility} data-testid='topMenu-toggle' />
+      </div>
     )
   }
 }
