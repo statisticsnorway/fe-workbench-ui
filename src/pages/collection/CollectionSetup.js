e016f5f7e42353c4fa5a4445e72545d384c84dee
mariannemellem <35998802+mariannemellem@users.noreply.github.com>
2019-10-23 10:36:49 +0200

    Develop collection setup config (#176)
    * Buttons in CollectionSetup
    
    * CollectionSetup: calls backend-api
    
    * Some parameterizing
    
    * Interval on uptime
    Functionality button-onclick
    
    * Added datacollectionspecs

--- src/pages/collection/CollectionSetup.js
+++ src/pages/collection/CollectionSetup.js
@@ -1,12 +1,149 @@
 import React, { Component } from 'react'
-import { Segment } from 'semantic-ui-react'
+import { Segment, Grid, Header, Button, Input, Select, Popup, Icon } from 'semantic-ui-react'
+import { COLLECTION_UI, COLLECTORS } from '../../utilities/enum/COLLECTIONADMIN'
+import { WorkbenchContext } from '../../context/ContextProvider'
+import { get, put, post } from '../../utilities/fetch/Fetch'
+import  skeFregPlaygroundSpec  from '../../mocks/collectorSpecs/skeFregPlaygroundSpec'
+import  skeSiriusPersonFastsattSpec  from '../../mocks/collectorSpecs/skeSiriusPersonFastsattSpec'
+import  tollTvinnTestSpec  from '../../mocks/collectorSpecs/tollTvinnTestSpec'
+
+const PUT = 'PUT'
+const POST = 'POST'
 
 class CollectionSetup extends Component {
+  static contextType = WorkbenchContext
+
+  state = {
+    collectorid: '',
+    uptime: ''
+  }
+
+  componentDidMount () {
+    this.setMockUptime();
+    this.interval = setInterval(() => {this.setMockUptime()}, 5000)
+  }
+
+  handleCollectorChange = (event, data) => {
+    this.setState({
+      collectorid: data.value,
+      collector: Object.keys(COLLECTORS).map(collector => COLLECTORS[collector]).filter(c => c.id === data.value)[0]
+    })
+    console.log(this.state.collector, 'collector i handleCollectorChange')
+  }
+
+  componentWillUnmount () {clearInterval((this.interval))}
+
+  onButtonClick = (url, verb, parameterfile) => {
+    let parameters = this.getParametersFromFile(parameterfile)
+    switch (verb) {
+      case PUT:
+        console.log('Url : ' +  url + ' Parameters: ' +  parameters)
+        return put(url, parameters).then(data => {
+          console.log(data)
+        })
+      case POST:
+        console.log('Url : ' +  url + ' Parameters: ' +  parameters)
+        return post(url, parameters).then(data => {
+          console.log(data)
+        })
+      default:
+        console.log('Url : ' +  url + ' Parameters: ' +  parameters)
+        return get(url, parameters).then(data => {
+          console.log(data)
+        })
+    }
+  }
+
+  getParametersFromFile (parameterFile) {
+    if (parameterFile === 'skeFregPlaygroundSpec') {
+      return JSON.stringify(skeFregPlaygroundSpec)
+    }
+    if (parameterFile === 'skeSiriusPersonFastsattSpec') {
+      return JSON.stringify(skeSiriusPersonFastsattSpec)
+    }
+    if (parameterFile === 'tollTvinnTestSpec') {
+      return JSON.stringify(tollTvinnTestSpec)
+    }
+  }
+
+  setMockUptime = () => {
+    return get("http://localhost:8092/mock-api/secondpassed").then(data => {
+      this.setState({uptime: data.timepassed})
+    })
+  }
 
   render () {
+    let context = this.context
+    const {collectorid, collector, uptime} = this.state
+    console.log(uptime)
+
+    const collectors = Object.keys(COLLECTORS).map(collector => ({
+      key: COLLECTORS[collector].id,
+      text: COLLECTORS[collector].name[context.languageCode],
+      value: COLLECTORS[collector].id
+    }))
+
+
+    const collectorStart = 'https://data-collector.staging-bip-app.ssb.no/task'
+    const converterStart = collector ? collector.converterUrl + 'rawdata-converter/start' : ''
+    const converterStop = collector ? collector.converterUrl + 'rawdata-converter/stop' : ''
+    const parameterFile = collector ? collector.collectorSpec : ''
+
     return (
       <Segment basic>
-        Placeholder page for menu item <b>CollectionSetup</b>
+        <Grid>
+          <Grid.Row>
+            <Header>
+              <Icon name={'database'} color={'blue'}/>
+              <Header.Content>{context.getLocalizedText(COLLECTION_UI.COLLECTION_HEADER)} for</Header.Content>
+            </Header>
+            <Select fluid
+                         style={{width: `${(8 * collectors.length) + 100}px`}}
+                         name='collector'
+                         placeholder={context.getLocalizedText(COLLECTION_UI.COLLECTORS)}
+                         value={collectorid}
+              // label={context.getLocalizedText(COLLECTORS)}
+                         options={collectors}
+                         onChange={this.handleCollectorChange}/>
+          </Grid.Row>
+          <Grid.Row>
+            {collector ? '(' + collector.converterUrl + ')' : ''}
+          </Grid.Row>
+          <Grid.Row>
+            <Grid>
+              <Grid.Row>
+                <Segment basic>
+                <Popup content={this.getParametersFromFile (parameterFile)}  trigger={
+                  <Button name='startcollector'
+                          onClick={() => this.onButtonClick(collectorStart, PUT, parameterFile)}
+                          disabled={!collector}
+                  >
+                    {context.getLocalizedText(COLLECTION_UI.BUTTON_START_COLLECTOR_FROM_BEGINNING.label)}
+                  </Button>
+                }
+                />
+                <Button name='startconverter'
+                             onClick={() => this.onButtonClick(converterStart, POST, parameterFile)}
+                             disabled={!collector}
+                >
+                  {context.getLocalizedText(COLLECTION_UI.BUTTON_START_CONVERTER.label)}
+                </Button>
+                <Button name='stopconverter'
+                             onClick={() => this.onButtonClick(converterStop, POST, parameterFile)}
+                             disabled={!collector}
+                >
+                  {context.getLocalizedText(COLLECTION_UI.BUTTON_STOP_CONVERTER.label)}
+                </Button>
+                </Segment>
+              </Grid.Row>
+              <Grid.Row>
+                <Segment basic>
+                  <Input label='Sekunder oppe' type="text" value={uptime} />
+                </Segment>
+              </Grid.Row>
+            </Grid>
+          </Grid.Row>
+        </Grid>
       </Segment>
     )
   }
---
+++ src/utilities/enum/COLLECTIONADMIN.js
@@ -0,0 +1,100 @@
+export const COLLECTION_UI = {
+  COLLECTION_HEADER: {
+    en: 'Collection administration',
+    nb: 'Innsamlingsadministrasjon'
+  },
+  COLLECTORS: {
+    en: 'Collectors',
+    nb: 'Innsamlinger'
+  },
+  BUTTON_START_COLLECTOR_FROM_BEGINNING: {
+    label: {
+      en: 'Start collector from beginning',
+      nb: 'Start innsamling fra begynnelsen'
+    },
+    hint: {
+      en: 'Start collector from beginning',
+      nb: 'Start innsamling fra begynnelsen'
+    }
+  },
+  BUTTON_PAUSE_COLLECTOR: {
+    label: {
+      en: 'Pause',
+      nb: 'Paus'
+    },
+    hint: {
+      en: 'Pause collection',
+      nb: 'Pause innsamling'
+    }
+  },
+  BUTTON_START_COLLECTOR_FROM_LAST: {
+    label: {
+      en: 'Start from last',
+      nb: 'Start fra forrige'
+    },
+    hint: {
+      en: 'Start from last',
+      nb: 'Start fra forrige'
+    }
+  },
+  BUTTON_STOP_COLLECTOR: {
+    label: {
+      en: 'Stop',
+      nb: 'Stopp'
+    },
+    hint: {
+      en: 'Stop collection',
+      nb: 'Stopp innsamling'
+    }
+  },
+  BUTTON_START_CONVERTER: {
+    label: {
+      en: 'Start converter',
+      nb: 'Start konvertering'
+    },
+    hint: {
+      en: 'Start converter',
+      nb: 'Stopp konvertering'
+    }
+  },
+  BUTTON_STOP_CONVERTER: {
+    label: {
+      en: 'Stop converter',
+      nb: 'Stopp konvertering'
+    },
+    hint: {
+      en: 'Stop converter',
+      nb: 'Stopp konvertering'
+    }
+  }
+}
+
+export const COLLECTORS = {
+  FREG: {
+    id: 1,
+    name: {
+      en: 'FREG',
+      nb: 'FREG'
+    },
+    converterUrl: 'https://freg-converter.staging-bip-app.ssb.no/',
+    collectorSpec: 'skeFregPlaygroundSpec'
+  },
+  SKATT: {
+    id: 2,
+    name: {
+      en: 'Skatteetaten.',
+      nb: 'Skatteetaten'
+    },
+    converterUrl: 'https://sirius-converter.staging-bip-app.ssb.no/',
+    collectorSpec: 'skeSiriusPersonFastsattSpec'
+  },
+  TVINN: {
+    id: 3,
+    name: {
+      en: 'Tvinn',
+      nb: 'Tvinn'
+    },
+    converterUrl: 'https://tvinn-converter.staging-bip-app.ssb.no/',
+    collectorSpec: 'tollTvinnTestSpec'
+  }
+}
