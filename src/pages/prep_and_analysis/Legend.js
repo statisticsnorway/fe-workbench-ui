8ff9aeaee36fe49d49df2f11bea1a195c84e5a48
Arild Johan Takvam-Borge <takvamborgen@users.noreply.github.com>
2019-11-15 09:18:44 +0100

    Bugfix/set correct legend size (#209)
    * Fix problem with legend elements not showing due to container size

--- src/pages/prep_and_analysis/Legend.js
+++ src/pages/prep_and_analysis/Legend.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Graph from "react-graph-vis"
 
 const options = {
@@ -18,11 +18,33 @@ const options = {
 };
 
 const Legend = (props) => {
+  const [ network, setNetwork ] = useState(null)
+  const [ graphContainer, setGraphContainer ] = useState(null)
+
+  useEffect( () => {
+    if (network) {
+      const width = graphContainer.clientWidth
+      network.moveTo({
+        position: {
+          x: 0,
+          y: 0
+        },
+        offset: {
+          x: -width/2,
+          y: 0
+        },
+        animation: {
+          duration: 800,
+          easingFunction: 'easeInOutQuad'
+        }
+      })
+    }
+  }, [network, graphContainer])
 
   return(
-    <div style={{height: '70px', width: '40%', float: 'left'}}>
+    <div style={{height: '70px', width: '100%', float: 'left'}} ref={ (graphContainer) => setGraphContainer(graphContainer)}>
       {props.legend &&
-      <Graph graph={props.legend} options={options}/>
+      <Graph graph={props.legend} options={options} getNetwork={network => setNetwork(network)}/>
       }
     </div>
   )
